068d69a47053038e1a2f8d81660c2bceea20bac5
aziz-codes <azizisoffline@gmail.com>
2023-08-31 20:40:41 +0500

    created data for keys that will return an array object of keys

---
+++ src/data/Keys.jsx
@@ -0,0 +1,63 @@
+import { TvIcon, PhoneIcon } from "@heroicons/react/24/outline";
+export const keys = [
+  {
+    label: "f1",
+    keyCode: "f1",
+  },
+  {
+    label: "f2",
+    keyCode: "f2",
+  },
+  {
+    label: "f3",
+    keyCode: "f3",
+  },
+  {
+    label: "f4",
+    keyCode: "f4",
+  },
+  {
+    label: "f5",
+    keyCode: "f5",
+  },
+  {
+    label: "f6",
+    keyCode: "f7",
+  },
+  {
+    label: "f8",
+    keyCode: "f8",
+  },
+  {
+    label: "f9",
+    keyCode: "f9",
+  },
+  {
+    label: "f10",
+    keyCode: "f10",
+  },
+  {
+    label: "f11",
+    keyCode: "f11",
+  },
+  {
+    label: "f12",
+    keyCode: "f12",
+  },
+  {
+    label: <TvIcon className="h-4 w-4" />,
+    keyCode: "",
+  },
+  {
+    label: <PhoneIcon className="h-4 w-4" />,
+    keyCode: "",
+  },
+  {
+    label: <PhoneIcon className="h-4 w-4 rotate-6" />,
+    keyCode: "",
+  },
+  {
+    label: "delete",
+    keyCode: "delete",
+  },
+];
