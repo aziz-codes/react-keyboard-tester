e01d7530a9ced487dca06a003ac8173f5566b6cf
aziz-codes <azizisoffline@gmail.com>
2023-09-01 19:48:46 +0500

    added isPressed property to all keys

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -2,8 +2,16 @@ import React, { useState } from "react";
 import { keys } from "../data/Keys";
 
 const Keyboard = () => {
-  const [allKeys, setAllKeys] = useState(keys);
+  const [allKeys, setAllKeys] = useState(
+    keys.map((key) => ({
+      label: key.label,
+      keyCode: key.keyCode,
+      isPressed: false,
+    }))
+  );
   console.log(allKeys);
+  const firstRow = allKeys.slice(0, 16);
+  const secondRow = allKeys.slice(16, 30);
   return (
     <section className="w-full  flex flex-col items-center mt-24">
       <div className="flex items-center gap-1">
@@ -14,7 +22,7 @@ const Keyboard = () => {
       <div className="w-full max-w-4xl  h-auto max-h-full border my-2 px-2 flex flex-col items-center justify-center py-2 gap-3">
         {/* first row */}
         <div className="flex gap-2 items-center">
-          {allKeys.slice(0, 16).map((btn, index) => (
+          {firstRow.map((btn, index) => (
             <button
               className={`h-6 w-10 border rounded-sm shadow-md text-xs text-center flex justify-center items-center`}
               key={index}
@@ -26,13 +34,13 @@ const Keyboard = () => {
         {/* end of first row */}
         {/* second row */}
         <div className="flex gap-2 items-center">
-          {allKeys.slice(16, 30).map((btn, index) => (
+          {secondRow.map((btn, index) => (
             <button
               key={index}
               className={`${
                 index === 0 && "h-10 w-8  rounded-sm"
               } h-10 w-11 border rounded-sm flex items-center justify-center ${
-                index === allKeys.slice(16, 30).length - 1 && "w-[82px] "
+                index === secondRow.length - 1 && "w-[96px] px-5"
               }`}
             >
               {btn.label}
