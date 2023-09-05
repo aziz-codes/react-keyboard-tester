66f67f54faf5ca4b2b2e4433ae57ca26e3ecdee0
aziz-codes <azizisoffline@gmail.com>
2023-09-05 21:30:12 +0500

    using global variable darkMode,rendering icons based on theme appereance

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,8 +1,26 @@
-import { MoonIcon } from "@heroicons/react/24/outline";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import { Store } from "../context/Context";
 const Appbar = () => {
+  const { darkMode, setDarkMode } = Store();
+  const handleToggle = () => {
+    setDarkMode((prev) => !prev);
+  };
+
   return (
     <div className="w-full h-11 flex items-center border border-b justify-end px-4">
-      <MoonIcon className="h-5 w-5 cursor-pointer" title="dark mode" />
+      {darkMode ? (
+        <SunIcon
+          className="h-5 w-5 cursor-pointer"
+          title="light mode"
+          onClick={handleToggle}
+        />
+      ) : (
+        <MoonIcon
+          className="h-5 w-5 cursor-pointer"
+          title="dark mode"
+          onClick={handleToggle}
+        />
+      )}
     </div>
   );
 };
