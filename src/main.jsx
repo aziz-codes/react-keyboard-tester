8631d5481f53e9fc3416d88c265db707014eec8a
aziz-codes <azizisoffline@gmail.com>
2023-09-05 21:31:00 +0500

    wrapped whole app with context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import { AppContextProvider } from "./context/Context.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <AppContextProvider>
     <App />
-  </React.StrictMode>
+  </AppContextProvider>
 );
