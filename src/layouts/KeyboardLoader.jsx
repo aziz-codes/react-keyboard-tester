e2492ed03da0cecacac9a3f86c5ba63c63efaee8
aziz-codes <azizisoffline@gmail.com>
2023-09-05 21:08:49 +0500

    Skeleton loader for keyboard

---
+++ src/layouts/KeyboardLoader.jsx
@@ -0,0 +1,9 @@
+const KeyboardLoader = () => (
+  <section className="h-full w-full flex justify-center items-center flex-col gap-4">
+    <div className="w-72 bg-gray-400 animate-pulse h-4"></div>
+    <div className="h-96 w-[400px] bg-gray-400 animate-pulse"></div>
+  </section>
+);
+
+export default KeyboardLoader;
+// this file will serve as a loader..
