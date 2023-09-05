ed232ed40a72260991c943b3a87bda23e9062fdf
aziz-codes <azizisoffline@gmail.com>
2023-09-05 20:58:59 +0500

    Created Appbar component for other settings

---
+++ src/components/Appbar.jsx
@@ -0,0 +1,9 @@
+const Appbar = () => {
+  return (
+    <div className="w-full h-11 flex items-center border border-b justify-end px-4">
+      Appbar
+    </div>
+  );
+};
+
+export default Appbar;
