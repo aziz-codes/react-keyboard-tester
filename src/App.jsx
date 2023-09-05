047a3b88dffd23d91df2e984f75621c6140d856e
aziz-codes <azizisoffline@gmail.com>
2023-09-05 20:58:16 +0500

    Rendered Appbar at top of Keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { lazy, Suspense } from "react";
+import Appbar from "./components/Appbar";
 const Keyboard = lazy(() => import("./components/Keyboard"));
 const App = () => {
   return (
     <Suspense fallback="Loading...">
+      <Appbar />
       <Keyboard />
     </Suspense>
   );
