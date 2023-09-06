6fcbaa974c79aad35ccf233e3fba53cd7b08fdcf
aziz-codes <azizisoffline@gmail.com>
2023-09-06 11:01:39 +0500

    added dark mode functionality, adjust layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { lazy, Suspense } from "react";
 import Appbar from "./components/Appbar";
 import KeyboardLoader from "./layouts/KeyboardLoader";
 const Keyboard = lazy(() => import("./components/Keyboard"));
+import { Store } from "./context/Context";
 const App = () => {
+  const { darkMode } = Store();
   return (
     <Suspense fallback={<KeyboardLoader />}>
-      <Appbar />
-      <Keyboard />
+      <section
+        className={`h-screen flex w-full flex-col gap-2 items-center justify-center ${
+          darkMode ? "bg-slate-800 text-white" : "bg-white text-black"
+        }`}
+      >
+        <Appbar />
+        <Keyboard />
+      </section>
     </Suspense>
   );
 };
